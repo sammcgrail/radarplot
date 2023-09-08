51f8eeca75b0dad9f41c33dd26114b5f06bf2405
Sam McGrail <sammcgrail@gmail.com>
2023-09-08 17:06:42 -0400

    add fetch, add NOAA token ref, get it to the point where it fetches but its returning non-json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RadarPage from "./RadarPage";
+
+const NOAA_API_BASE_URL = "/cdo-web/api/v2/locations"; // Replace with the actual NOAA endpoint you're using
+const NOAA_TOKEN = process.env.REACT_APP_NOAA_TOKEN;
+
+const fetchRadarData = async () => {
+  const response = await fetch(NOAA_API_BASE_URL, {
+    headers: {
+      Authorization: `Bearer ${NOAA_TOKEN}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const contentType = response.headers.get("content-type");
+  if (!contentType || !contentType.includes("application/json")) {
+    throw new Error("Received non-JSON response.");
+  }
+
+  return await response.json();
+};
 
 function App() {
+  useEffect(() => {
+    let isMounted = true; // track mounting status
+
+    const fetchData = async () => {
+      try {
+        const radarData = await fetchRadarData();
+        if (isMounted) {
+          console.log(radarData);
+        }
+      } catch (error) {
+        console.error("Error fetching radar data:", error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false; // cleanup on unmounting
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<RadarPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
