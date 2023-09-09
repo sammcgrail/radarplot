673690ff0f93f626ece17236ed329b9c91388c33
MichaelJOHare <mjohare13@gmail.com>
2023-09-09 00:44:22 -0400

    Added basic structure for rendering data collected from fetchRadarData to page

--- src/RadarPage.js
+++ src/RadarPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function RadarPage() {
+function RadarPage({ data }) {
   // Pass radar data here as a prop or using context,
   // Just static message for now
   // wrap this in a try catch and show a loading... spinner - then actually render it on page later
@@ -9,7 +9,21 @@ function RadarPage() {
     <div className="radar-container">
       <h2>Radar Data</h2>
       <p>We've fetched some radar data! Check your console for the details.</p>
-      {/* If you were to display some of that data, you'd iterate and render it here. */}
+      {data.length ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <h3>{item.name}</h3>
+              <p>ID: {item.id}</p>
+              <p>Data Coverage: {item.datacoverage}</p>
+              <p>Max Date: {item.maxdate}</p>
+              <p>Min Date: {item.mindate}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p> // Display loading message until data is fetched
+      )}
     </div>
   );
 }
