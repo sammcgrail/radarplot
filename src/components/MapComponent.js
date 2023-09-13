6b21bfe1329113c9e115a5c5a43fe3d6d7abbb07
Tyler <nlax76@gmail.com>
2023-09-12 21:43:00 -0400

    add client token errors (maintains console errors) (#23)
    * add client token errors (maintains console errors)
    
    * fixing mis-merge
    
    * fix `ServerError` copy if no `errorSource`

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import ServerError from "./ServerError";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-if (MAPBOX_TOKEN == null || MAPBOX_TOKEN === "") {
-  throw new Error(
-    `'REACT_APP_MAPBOX_ACCESS_TOKEN' must be defined in .env.local.`
-  );
-}
 
 function MapComponent({ initialViewState, mapStyle }) {
-  return (
+  return MAPBOX_TOKEN ? (
     <Map
       mapboxAccessToken={MAPBOX_TOKEN}
       initialViewState={initialViewState}
       style={{ width: 800, height: 600 }}
       mapStyle={mapStyle}
+    />
+  ) : (
+    <ServerError
+      errorSource="MapBox"
+      error="'REACT_APP_MAPBOX_ACCESS_TOKEN' must be defined in .env.local"
     />
   );
 }
