e8a1afc47f845e7f01f4f921df3e5b2c131c38f9
br-ndt <nlax76@gmail.com>
2023-09-12 21:30:59 -0400

    fixing mis-merge

--- src/App.js
+++ src/App.js
@@ -2,78 +2,16 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import LoadingSpinner from "./components/LoadingSpinner";
 import RadarPage from "./RadarPage";
-import ServerError from "./components/ServerError";
 import WeatherPage from "./WeatherPage";
 
-const NOAA_API_BASE_URL = "/cdo-web/api/v2/locations";
-// TODO: This should not be shipped to the browser.
-const NOAA_TOKEN = process.env.REACT_APP_NOAA_TOKEN;
-
-const fetchRadarData = async () => {
-  console.log(`Fetching data from: ${NOAA_API_BASE_URL}`);
-
-  if (!NOAA_TOKEN) {
-    throw new Error(`'REACT_APP_NOAA_TOKEN' must be defined in .env.local.`);
-  }
-  const response = await fetch(NOAA_API_BASE_URL, {
-    headers: {
-      token: NOAA_TOKEN,
-    },
-  });
-  if (response.redirected) {
-    throw new Error("Request was redirected. Possible authentication issue.");
-  }
-  if (!response.ok) {
-    const text = await response.text();
-    throw new Error(`HTTP error! Status: ${response.status}, Body: ${text}`);
-  }
-
-  return await response.json();
-};
-
 function App() {
-  const [radarData, setRadarData] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    let isMounted = true; // track mounting status
-
-    const fetchData = async () => {
-      try {
-        const radarData = await fetchRadarData();
-        if (isMounted) {
-          setRadarData(radarData.results);
-        }
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      isMounted = false; // cleanup on unmounting
-    };
-  }, []);
   return (
     <Router>
       <Header />
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={
-              error ? (
-                <ServerError errorSource="NOAA" error={error} />
-              ) : radarData?.length ? (
-                <RadarPage data={radarData} />
-              ) : (
-                <LoadingSpinner />
-              )
-            }
-          />
+          <Route path="/" element={<RadarPage />} />
           <Route path="/weather" element={<WeatherPage />} />
         </Routes>
       </div>
--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
+import ServerError from "./ServerError";
 
 const NOAA_API_BASE_URL = "/cdo-web/api/v2/locations";
 const NOAA_TOKEN = process.env.REACT_APP_NOAA_TOKEN;
 
 const fetchRadarData = async () => {
+  if (!NOAA_TOKEN) {
+    throw new Error("'REACT_APP_NOAA_TOKEN' must be defined in .env.local");
+  }
   const response = await fetch(NOAA_API_BASE_URL, {
     headers: {
       token: NOAA_TOKEN,
@@ -20,9 +24,10 @@ const fetchRadarData = async () => {
 
 function WeatherPanel() {
   const [radarData, setRadarData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    let isMounted = true;
+    let isMounted = true; // track mounting status
 
     const fetchData = async () => {
       try {
@@ -31,33 +36,37 @@ function WeatherPanel() {
           setRadarData(radarData.results);
         }
       } catch (error) {
-        console.error("Error fetching radar data:", error);
+        setError(error.message);
       }
     };
 
     fetchData();
 
     return () => {
-      isMounted = false;
+      isMounted = false; // cleanup on unmounting
     };
   }, []);
 
   return (
     <div>
-      <h2>Radar Data</h2>
-      <p>We've fetched some radar data!</p>
-      {radarData.length ? (
-        <ul>
-          {radarData.map((item) => (
-            <li key={item.id}>
-              <h3>{item.name}</h3>
-              <p>ID: {item.id}</p>
-              <p>Data Coverage: {item.datacoverage}</p>
-              <p>Max Date: {item.maxdate}</p>
-              <p>Min Date: {item.mindate}</p>
-            </li>
-          ))}
-        </ul>
+      {error ? (
+        <ServerError errorSource="NOAA" error={error} />
+      ) : radarData?.length ? (
+        <>
+          <h2>Radar Data</h2>
+          <p>We've fetched some radar data!</p>
+          <ul>
+            {radarData.map((item) => (
+              <li key={item.id}>
+                <h3>{item.name}</h3>
+                <p>ID: {item.id}</p>
+                <p>Data Coverage: {item.datacoverage}</p>
+                <p>Max Date: {item.maxdate}</p>
+                <p>Min Date: {item.mindate}</p>
+              </li>
+            ))}
+          </ul>
+        </>
       ) : (
         <LoadingSpinner />
       )}
