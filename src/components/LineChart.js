73cd945b71a0d23ffa2ca61e5b0012f0f529d1ee
Tyler <nlax76@gmail.com>
2023-09-12 23:36:38 -0400

    Add line graph to `/weather` (#25)
    * add `chart.js` and `react-chartjs-2`
    
    * add `LineChart`
    
    * update `TodayWeather` to show temp graph

---
+++ src/components/LineChart.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
+
+export const options = {
+  responsive: true,
+  scales: {
+    x: {
+      ticks: {
+        color: "#000",
+      },
+    },
+  },
+};
+
+export default function LineChart({ data }) {
+  return (
+    <div>
+      <Line {...{ data, options }} />
+    </div>
+  );
+}
