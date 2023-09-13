88b3a66cfd1605fa62c3952ba753a99a34c18e3a
br-ndt <nlax76@gmail.com>
2023-09-12 22:57:06 -0400

    update `TodayWeather` to show temp graph

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -3,10 +3,30 @@ import { useLocation } from "../contexts/LocationContext";
 import getDateString from "../helpers/getDateString";
 
 import styles from "../styles/TodayWeather.module.css";
+import LineChart from "./LineChart";
 
 export default function TodayWeather() {
   const { locationData, weatherData } = useLocation();
   const { country, name, state } = locationData[0];
+
+  const nextDayHours = weatherData?.hourly?.slice(0, 25);
+  // this should probably be memoized or something, but w/e
+  const chartData = nextDayHours
+    ? {
+        labels: nextDayHours?.map(
+          (hour) => getDateString(hour.dt)?.split(" at ")[1]
+        ),
+        datasets: [
+          {
+            label: "Temperature (F)",
+            data: nextDayHours?.map((hour) => hour.temp),
+            yAxisID: "y-axis-temp",
+            backgroundColor: "rgba(180, 0, 255, 0.8)",
+            borderColor: "#000",
+          },
+        ],
+      }
+    : null;
 
   return (
     <>
@@ -14,19 +34,17 @@ export default function TodayWeather() {
       <h3>
         {name}, {state} {country}
       </h3>
-      <ul>
-        {weatherData?.hourly?.slice(0, 25).map((hour, i) => {
+      {chartData && <LineChart data={chartData} />}
+      <ul className={styles.timeList}>
+        {nextDayHours?.map((hour, i) => {
           const dateTime = getDateString(hour.dt);
           const timeString = dateTime?.split(" at ")[1];
-
           return (
-            <ul className={styles.timeList} key={`weather-hour-${i}`}>
-              <p>
-                <li>{timeString}</li>
-                <li>{hour.temp}F</li>
-                <li>{hour.weather[0].description}</li>
-              </p>
-            </ul>
+            <div key={`weather-hour-${i}`} className={styles.timeEntry}>
+              <p>{timeString}</p>
+              <p>{hour.temp}F</p>
+              <p>{hour.weather[0].description}</p>
+            </div>
           );
         })}
       </ul>
